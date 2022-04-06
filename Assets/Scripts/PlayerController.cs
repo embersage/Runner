64f8b3e35eb32440b16089b8a6cca00689ed4d93
Van <i.molodtsov03@list.ru>
2022-04-06 23:52:53 +0500

    Добавлен общий баланс монет в главном меню

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour
                 Destroy(obstacle.gameObject);
             else
             {
-                Time.timeScale = 0;
+                Time.timeScale = 0;                
                 int lastRunScore = int.Parse(scoreScript.scoreText.text.ToString());
                 PlayerPrefs.SetInt("lastRunScore", lastRunScore);
                 losePanel.SetActive(true);            
@@ -40,6 +40,7 @@ public class PlayerController : MonoBehaviour
         if (bonus.gameObject.tag == "Coin")
         {
             coinsCount++;
+            PlayerPrefs.SetInt("coins", coinsCount);
             coinsText.text = coinsCount.ToString();
             Destroy(bonus.gameObject);
         }
@@ -90,6 +91,8 @@ public class PlayerController : MonoBehaviour
         IsImmortal = false;
         ObstacleController.speed = 30f;
         PlayerPrefs.SetInt("recordScore", 0);
+        coinsCount = PlayerPrefs.GetInt("coins");
+        coinsText.text = coinsCount.ToString();
     }
 
     private void FixedUpdate()
